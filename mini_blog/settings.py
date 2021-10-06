73ef7212c70a38c798d078de559a1b51ae1a9afa
Nikhil777jais <nikhil777jais@gmail.com>
2021-10-06 19:34:58 +0530

    Db changed to Myql

--- mini_blog/settings.py
+++ mini_blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mini_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mini_blog',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
